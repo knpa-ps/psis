4ac1165d2a246a4bc271bdf2b1d4bfdf097ac0cf
Choe, Young-Woo <projet6634@gmail.com>
2014-04-22 11:11:49 +0900

    sync

---
+++ app/views/widget/user-selector.blade.php
@@ -0,0 +1,9 @@
+<div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	<h3>사용자 선택</h3>
+</div>
+<div class="modal-body">
+Hello world!
+</div>
+
+
